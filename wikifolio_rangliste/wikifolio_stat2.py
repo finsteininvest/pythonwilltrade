ee6e251b9617df5380b288516cd19ada4ad1b00b
finsteininvest <59108763+finsteininvest@users.noreply.github.com>
2020-04-05 10:27:52 +0200

    Add files via upload

--- wikifolio_rangliste/wikifolio_stat2.py
+++ wikifolio_rangliste/wikifolio_stat2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 	wikifolio_stat2.py
 	
@@ -20,7 +21,7 @@ import re
 import argparse
 from prettytable import PrettyTable
 
-# Parameter für die Kommandozeile definieren
+# Parameter fuer die Kommandozeile definieren
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file', help='Wikifolio HTML file', required=True)
 args = parser.parse_args()
@@ -28,35 +29,34 @@ args = parser.parse_args()
 # HTML Datei parsen
 soup = BeautifulSoup(open(args.file, "r"), "html.parser")
 
-# Einzelne Wikifolio Blöcke in der Datei identifizieren.
-wikifolios = soup.find_all("div", class_="container-fluid wikifolio-preview js-watchlist-wikifoliopreview c-search-card")									  
+# Einzelne Wikifolio Bloecke in der Datei identifizieren.
+wikifolios = soup.find_all("div", class_="js-wf-preview__root")									  
 
-# Tabelle für die Ausgabe vorbereiten
-t = PrettyTable(['Rang','ISIN', 'Name', 'Investiert', '1M Perf.'])
+# Tabelle fuer die Ausgabe vorbereiten
+t = PrettyTable(['Rang','ISIN', 'Name', 'Investiert', 'Ø Perf.'])
 rang = 1
 
-# Für jedes Wikifolio die Werte auslesen
+# Fuer jedes Wikifolio die Werte auslesen
 for wikifolio in wikifolios:
-	name = wikifolio.find("span", class_="js-wikifolio-shortdescription")
+	name = wikifolio.find("a", class_="c-icon-name__text u-fw-sb gtm-wfpreview__title")
 	# isin wird nicht in der Tabelle ausgegeben.
 	isin = wikifolio.find("span", class_="js-copy-isin")
-	investiert = wikifolio.find_all("span", class_="pull-right text-small text-bold rightspan")
-	investiert_eur = investiert[5].text.replace('EUR', '')
+	investiert = wikifolio.find_all("span", class_="c-strike__value")
+	investiert_eur = investiert[7].text.replace('EUR', '')
 	wikifolio_name = name.text
-
-	# Regulären Ausdruck verwenden, um Performance
-	# zu ermitteln. Der Eintrag ändert sich, je nachdem ob
+	# Regulaeren Ausdruck verwenden, um Performance
+	# zu ermitteln. Der Eintrag aendert sich, je nachdem ob
 	# die Performance positiv oder negativ ist.
-	perf_pattern= re.compile("pull-right text-small text-bold rightspan u-.*")
-	perf = wikifolio.find_all("span", perf_pattern)
-
+	#perf_pattern= re.compile("pull-right text-small text-bold rightspan u-.*")
+	perf = wikifolio.find_all("span", class_="c-strike__value")
+	perf = perf[2].text
 	# Gefundene Werte in Tabelle eintragen
-	t.add_row([str(rang), isin.text, wikifolio_name, investiert_eur, perf[1].text])
+	t.add_row([str(rang), isin.text, wikifolio_name, investiert_eur, perf])
 	t.align['Name'] = 'l'
 	t.align['ISIN'] = 'l'
 	t.align['Rang'] = 'r'
 	t.align['Investiert'] = 'r'
-	t.align['1M Perf.'] = 'r'
+	t.align['Ø Perf.'] = 'r'
 	rang += 1
 
 # Tabelle ausgeben
