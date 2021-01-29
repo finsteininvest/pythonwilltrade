ef5aafcd58cb695e4b1008a05a1b2971badf98e6
finsteininvest <59108763+finsteininvest@users.noreply.github.com>
2021-01-29 09:17:31 +0100

    Added api_key

--- von_guenstig_bis_nicht/get_sp_500_dcf.py
+++ von_guenstig_bis_nicht/get_sp_500_dcf.py
@@ -23,22 +23,26 @@ args = parser.parse_args()
 symbole_df = pd.read_csv(args.symbols_file)
 
 dcf_delta_list = []
+api_key = ''
 
 for index, row in symbole_df.iterrows():
     ticker = row['ticker']
     name = row['name']
-    dcf_json = requests.get(f"https://financialmodelingprep.com/api/v3/company/discounted-cash-flow/{ticker}")
+    dcf_json = requests.get(f"https://financialmodelingprep.com/api/v3/company/discounted-cash-flow/{ticker}?apikey={api_key}")
     dcf_json = dcf_json.json()
     # Nicht für jeden Wert gibt es eine DCF Berechnung
     try:
         delta = float(dcf_json['dcf']) - float(dcf_json['Stock Price'])
         delta_prc = delta/float(dcf_json['Stock Price'])*100
-        print(f"{name}, DCF: {dcf_json['dcf']}, latest close: {dcf_json['Stock Price']}, Delta: {delta}, {delta_prc}%")
-        dcf_delta_list.append([name, ticker, dcf_json['dcf'], dcf_json['Stock Price'], delta, delta_prc])
+        delta = float(f'{delta:.2f}')
+        delta_prc = float(f'{delta_prc:.2f}')
+        dcf = float(f"{dcf_json['dcf']:.2f}")
+        print(f"{name}, DCF: {dcf}, latest close: {dcf_json['Stock Price']}, Delta: {delta}, {delta_prc}%")
+        dcf_delta_list.append([name, ticker, dcf, dcf_json['Stock Price'], delta, delta_prc])
         sleep(1)
     except:
         pass
 
-dcf_delta_df = pd.DataFrame.from_records(dcf_delta_list, columns=['name','ticker','dcf', 'latest', 'delta', '%'])
+dcf_delta_df = pd.DataFrame.from_records(dcf_delta_list, columns=['name','ticker','dcf', 'latest close', 'delta', '%'])
 dcf_delta_df = dcf_delta_df.sort_values(by=['%'], ascending = False)
 dcf_delta_df.to_csv('sp_500_dcf.csv', sep = ';', decimal = ',')
